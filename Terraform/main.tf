5fc8b6fa9e9fbb825258f62b57e49d4f571d1b22
EC2 Default User <suhrob7072gmail.com>
2023-04-01 16:54:48 +0000

    Updated

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -89,7 +89,7 @@ resource "aws_instance" "myapp-server" {
     availability_zone = var.avail_zone
 
     associate_public_ip_address = true
-    key_name = "ethan-key"
+    key_name = "Key"
     user_data = file("entry-script.sh")
 
     tags = {
